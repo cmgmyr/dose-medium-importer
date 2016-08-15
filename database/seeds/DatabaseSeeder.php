41cd83bc0899161b1c1c43de3b46bda5c391edd2
Chris Gmyr <cmgmyr@gmail.com>
2016-08-15 09:44:55 -0400

    add Pending model and migration

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Pending;
 use App\User;
 use Illuminate\Database\Seeder;
 use League\Csv\Reader;
@@ -24,6 +25,19 @@ class DatabaseSeeder extends Seeder
             User::create([
                 'name' => trim($user[0]),
                 'medium_token' => trim($user[1]),
+            ]);
+        });
+
+        Pending::truncate();
+
+        $reader = Reader::createFromPath(base_path('data/ids.csv'));
+
+        // skip the header
+        $all = $reader->setOffset(1)->fetchAll();
+
+        collect($all)->each(function ($article) {
+            Pending::create([
+                'article_id' => trim($article[0]),
             ]);
         });
     }
